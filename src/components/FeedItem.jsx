8ca4cd31c86b86b1c3ad10a8f4b8b7bc5b065528
Student <example@example.com>
2019-11-19 11:30:59 -0800

    Add FeedItem.

--- src/components/FeedItem.jsx
+++ src/components/FeedItem.jsx
@@ -1,48 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function FeedItem(props){
-    var imgStyle = {
-        height: '50px',
-        width: '250px'
-           
-       };
-      
-       var cardStyle = {
-        height: '450px',
-        width: '250px',
-        marginRight: '40px',
-        marginTop:'15px'
-       };
+function FeedItem (props)
+{
+     function handleLikeClick = () => {
+        props.onNewLike(props.count);
+      }
+
+      function handleDisLikeClick = () => {
+        props.onNewDisLike(props.count);
+      }
+
 
     return(
     
         
     <div className="card" style ={cardStyle} >
-    <img src={props.img} className="card-img-top" style ={imgStyle} alt="red"/>
-    <div className="card-body">
-    <h5 className="card-title">{props.title}</h5>
-   
+    {/* <img src={props.img} className="card-img-top" style ={imgStyle} alt="red"/> */}
+        <div className="card-body">
+            <h5 className="card-title">{props.title}</h5>
+            <p>{props.content}</p>
+            <p>Likes: {props.likes}<button onClick={this.handleLikeClick}></button> </p>
+            <p>Dislikes: {props.dislikes}<button onClick={this.handleDislikeClick}></button> </p>   
+        </div>
     </div>
-    <ul className="list-group list-group-flush">
-    {/* <li className="list-group-item">Up Vote: {props.upVote}</li> */}
-    <button type="button" class="btn btn-secondary" onclick="myFunction(var newPintsLeft)">Up Vote</button>
-
-    {/* <li className="list-group-item">DownVote: {props.downVote}</li> */}
-    <button type="button" class="btn btn-secondary" onclick="myFunction(var newPintsLeft)">Down Vote</button>
-    </ul>
-    <div className="card-body">
-  
-   
-   </div>
-  </div>
     );
-}
+
+    }
 
 FeedItem.propTypes = {
-    img: PropTypes.element,
+    // img: PropTypes.element,
     title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+    count: PropTypes.number,
+    like: PropTypes.number,
+    dislikes: PropTypes.number,
     feed: PropTypes.string.isRequired
 };
+    
 
 export default FeedItem;
--- src/components/NewsFeed.jsx
+++ src/components/NewsFeed.jsx
@@ -2,21 +2,37 @@ import React from 'react';
 import NewPost from './NewPost';
 import PropTypes from 'prop-types';
 
-function NewsFeed(props) {
-    return (
-        <div>
-            <hr />
-            {props.newsFeedList.map((post, index) =>
-                <NewPost userName={post.userName}
-                    feed={post.feed}
-                    key={index} />
-            )}
-        </div>
-    );
-}
+class NewsFeed extends React.Component 
+{
+    constructor(props) {
+        super(props);
+        //super accesses the React.Component constructor
+        this.state = {
+          //all state value key-value pairs go here
+          newsFeedList: [
+            {
+                
+                count: 0,
+              userName: "name1",
+              feed: 'feed1'
+            }
+          ],
+          currentSearchTerm: "",
+        };
 
-NewsFeed.propTypes = {
-    newsFeedList: PropTypes.array
-};
+        render(){
+        return (
+            <div>
+                <hr />
+                {props.newsFeedList.map((post, index) =>
+                    <NewsFeed title={post.title}
+                        feed={post.feed}
+                        key={index} />
+                )}
+            </div>
+        );
 
+        
+       
+       
 export default NewsFeed;
